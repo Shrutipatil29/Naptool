9d7c76e376a6720333272fe1221bca233e416721
Shrutipatil29 <154224704+Shrutipatil29@users.noreply.github.com>
2024-01-08 19:04:44 +0530

    Add files via upload

---
+++ Arise/src/test/java/test/RemoveProductFromCartTest.java
@@ -0,0 +1,66 @@
+package test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import org.testng.annotations.*;
+import org.testng.annotations.Listeners;
+import pojo.browser;
+import pom.BuyProductFromDescriptionPage;
+import pom.CartPage;
+import pom.NaptoolHomePage;
+import pom.ProductResultPage;
+import utility.Reports;
+
+@Listeners(test.Listeners.class)
+
+public class RemoveProductFromCartTest extends BaseTest {
+    ExtentReports extentReports;
+    ExtentTest extentTest;
+
+    @BeforeTest
+    public  void CreateReports()
+    {
+        extentReports= Reports.generateReports();
+    }
+
+    @BeforeMethod
+    public void openChrome() {
+        driver = browser.chrome();
+    }
+
+    @Test
+    public void verifyIfUserIsAbleToRemoveProductFromCart() throws InterruptedException {
+
+        extentTest=extentReports.createTest("verifyIfUserIsAbleToRemoveProductFromCart");
+
+        NaptoolHomePage naptolHomePage = new NaptoolHomePage(driver);
+        naptolHomePage.enterInSearchTab("Mobile");
+        naptolHomePage.clickOnSearch();
+
+        ProductResultPage productResultPage = new ProductResultPage(driver);
+        productResultPage.clickOnProductDescription(1);
+
+        BuyProductFromDescriptionPage buyProductFromDescriptionPage = new BuyProductFromDescriptionPage(driver);
+
+        buyProductFromDescriptionPage.switchToPage(driver, "https://www.naaptol.com/smart-watches/bluetooth-calling-smart-watch-with-neckband-and-mobile-stand-sc6/p/12612081.html");
+        buyProductFromDescriptionPage.clickOnClickHereToBuy();
+
+        CartPage cartPage= new CartPage(driver);
+
+        Thread.sleep(5000);
+
+        cartPage.ClickOnRemoveProductFromCart(driver);
+
+
+
+
+    }
+    @AfterTest
+
+    public void flushReports()
+    {
+        extentReports.flush();
+    }
+
+
+}
