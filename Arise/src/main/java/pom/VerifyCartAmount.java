3343523dbf5ac9545f85cf5d9b5465c6b903ebb5
Shrutipatil29 <154224704+Shrutipatil29@users.noreply.github.com>
2024-01-17 22:39:36 +0530

    Create VerifyCartAmount.java

---
+++ Arise/src/main/java/pom/VerifyCartAmount.java
@@ -0,0 +1,44 @@
+package pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VerifyCartAmount {
+
+        @FindBy(xpath = "//a[@href=\"/smart-watches/bluetooth-calling-smart-watch-with-neckband-and-mobile-stand-sc6/p/12612081.html\"]")private WebElement mobileName;
+        @FindBy(xpath = "(//li[@class='head_ship'])[2]")private WebElement ShipPrice;
+        @FindBy(xpath = "(//li[@class='head_UPrice'])[2]")private WebElement UnitPrice;
+        @FindBy(xpath = "(//li[@class='head_Amount'])[2]")private WebElement OrderAmount;
+        @FindBy(xpath = "(//li//label)[1]")private WebElement CartAmount;
+
+        public VerifyCartAmount(WebDriver driver) {
+            PageFactory.initElements(driver, this);
+        }
+
+        public String getProdName(){
+
+            return mobileName.getText();
+        }
+
+        public double getUnitPrice() {
+
+            return Double.parseDouble(UnitPrice.getText().substring(3));
+        }
+
+        public double getShippingPrice() {
+            return Double.parseDouble(ShipPrice.getText().substring(3));
+        }
+
+        public double getOrderAmount() {
+
+            return Double.parseDouble(OrderAmount.getText());
+        }
+
+        public double getCartAmount(){
+
+            return Double.parseDouble(CartAmount.getText().substring(3));
+        }
+
+}
