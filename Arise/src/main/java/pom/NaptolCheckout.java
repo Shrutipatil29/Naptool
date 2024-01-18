2027f5901b3026bf5825c19ca125cd57c86c55a9
Shrutipatil29 <154224704+Shrutipatil29@users.noreply.github.com>
2024-01-18 16:53:12 +0530

    Update NaptolCheckout.java

--- Arise/src/main/java/pom/NaptolCheckout.java
+++ Arise/src/main/java/pom/NaptolCheckout.java
@@ -1,86 +1,105 @@
-package test;
+package pom;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-import pojo.browser;
-import pom.NaptolCheckout;
-import pom.NaptoolHomePage;
-import pom.ProductQuickViewPage;
-import pom.ProductResultPage;
-import utility.Reports;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static test.BaseTest.driver;
+import java.time.Duration;
 
-public class NaptolCheckoutTest {
+public class NaptolCheckout {
 
-    private String name;
-    ExtentReports extentReports;
-    ExtentTest extentTest;
+    @FindBy(xpath = "//a[@id='login-panel-link']")private WebElement LogIn;
+    @FindBy(xpath = "//input[@id='registration-basic-panel-mobile']")private WebElement EnterMobileNumber;
+    @FindBy(xpath = "//input[@id='registration-basic-panel-submit']") private WebElement Continue;
+    @FindBy(xpath = "//input[@id='registration-otp-panel-submit']")private WebElement Submit;
 
-    @BeforeTest
-    public  void CreateReports()
-    {
-        extentReports= Reports.generateTestReports();
+    @FindBy(xpath = "(//div//b)[1]")private WebElement loginUserName;
+
+    @FindBy(xpath = "//a[@class='red_button2']")private WebElement Checkout;
+
+    @FindBy(xpath = "//select[@class='input_Special']")private WebElement title;
+    @FindBy(xpath = "//input[@name='firstName']")private WebElement firstName;
+    @FindBy(xpath = "//input[@name='lastName']")private WebElement LastName;
+    @FindBy(xpath = "//textarea[@name='address']")private WebElement Address;
+    @FindBy(xpath = "//input[@name='pincode']")private WebElement pincode;
+    @FindBy(xpath = "//input[@name='mobile']")private WebElement mobileNumber;
+    @FindBy(xpath = "//a[@id='addEditButton']")private WebElement saveAndProceed;
+
+    @FindBy(xpath = "//a[@class='button_2']")private WebElement shipToThisAddress;
+    @FindBy(xpath = "//a[@class='red_button_normal']")private WebElement placeOrder;
+    @FindBy (xpath= "(//td//span)[6]")private WebElement orderProductPrice;
+
+
+
+    public NaptolCheckout(WebDriver driver) {
+        PageFactory.initElements(driver, this);
     }
 
 
-    @BeforeMethod
-    public void openApplication() {
+    public void RegisterLoginInNaptol(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
+        wait.until(ExpectedConditions.visibilityOf(LogIn));
+        LogIn.click();
 
-        driver= browser.chrome();
+    }
+
+    public void EnterMobileNumber() throws InterruptedException {
+        EnterMobileNumber.sendKeys("9131301718");
+        Continue.click();
+
+        Thread.sleep(50000);
+        Submit.click();
+
+    }
+
+    public String getUserName(){
+
+         String userName= loginUserName.getText();
+
+         return userName;
+
+    }
+
+    public void clickOnCheckout(){
+
+        Checkout.click();
+    }
+
+    public void selectShippingAddress(){
+
+        Select titleSelect = new Select(title);
+
+        // Select the desired title by visible text
+        titleSelect.selectByVisibleText("Miss.");
+        firstName.sendKeys("shruti");
+        LastName.sendKeys("patil");
+        Address.sendKeys("LALUK");
+        pincode.sendKeys("784160");
+
+
+        mobileNumber.sendKeys("9131301718");
+        saveAndProceed.click();
+
+        shipToThisAddress.click();
+
+
+    }
+
+    public void placeOrderNow(WebDriver driver){
+        WebDriverWait wait= new WebDriverWait(driver, Duration.ofMillis(5000));
+        wait.until(ExpectedConditions.visibilityOf(placeOrder));
+        placeOrder.click();
+    }
+
+    public double getOrderAmount() {
+        return Double.parseDouble(orderProductPrice.getText().replace("/-", ""));
     }
 
 
-    @Test
-    public void NaptolLoginTest() throws InterruptedException {
-
-        extentTest=extentReports.createTest("NaptolLoginTest");
-
-        NaptolCheckout naptollogin= new NaptolCheckout(driver);
-        naptollogin.RegisterLoginInNaptol(driver);
-        naptollogin.EnterMobileNumber();
-        name=naptollogin.getUserName();
-        System.out.println(name);
-
-        Thread.sleep(5000);
-
-        ProductResultPage productResultPage =new ProductResultPage(driver);
-        ProductQuickViewPage productQuickViewPage =new ProductQuickViewPage(driver);
-
-        NaptoolHomePage naptoolHomePage = new NaptoolHomePage(driver);
-        naptoolHomePage.enterInSearchTab("Mobiles");
-        naptoolHomePage.clickOnSearch();
-
-
-        productResultPage.clickOnQuickView(driver, 0);
-        productQuickViewPage.clickOnClickHereToBuy();
-
-        naptollogin.clickOnCheckout();
-        naptollogin.selectShippingAddress();
-
-        naptollogin.placeOrderNow(driver);
-
-        System.out.println(naptollogin.getOrderAmount());
-
-
-        Assert.assertEquals(naptollogin.getOrderAmount(),800);
-
-
-
-
-
-    }
-    @AfterTest
-
-    public void flushReports()
-    {
-        extentReports.flush();
-    }
 
 
 }
